8722922fb4c8f4f801669df78d2523e3eee44f08
Pavel Angelov <pavel@st6.io>
2022-09-14 16:14:48 +0300

    docs: add game rules

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello world');
+// https://bicyclecards.com/how-to-play/sixty-six/
+// Determine who's the dealer
+// Deal cards
+// Determing the Trump
+
+// The non-dealer player is "PlayerA"
+// The dealer player is "PlayerB"
+
+// Trick
+//      Determine the mode (normal, closed)
+//      Announcements - marriage, change trump 9
+
+//      PlayerA plays
+//      Validate
+
+//      PlayerB plays
+//      Validate
+
+//      Determine first player (A or B) for next round
+
+//      Last trick points (10) when mode is normal
+
+// Score points
